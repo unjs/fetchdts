f97e867dadb243a4d20ba53753f3271352583c22
Daniel Roe <daniel@roe.dev>
2025-01-31 11:42:57 +0000

    build: use `.mjs` extension for built files

--- build.config.ts
+++ build.config.ts
@@ -1,13 +1,13 @@
-import { cp, readdir, rm } from 'node:fs/promises'
+import { readdir, rm } from 'node:fs/promises'
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
+  declaration: 'node16',
   hooks: {
     'rollup:done': async function () {
       // default to .js and .d.ts extensions
       for (const file of await readdir('dist')) {
-        if (file.endsWith('.mjs') || file.endsWith('.d.mts')) {
-          await cp(`dist/${file}`, `dist/${file.replace('.mjs', '.js').replace('.d.mts', '.d.ts')}`)
+        if (file.endsWith('.d.cts')) {
           await rm(`dist/${file}`)
         }
       }
