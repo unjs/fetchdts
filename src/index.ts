f8d77a72aa0f706c781359cd2bc7d394f2078dc9
Daniel Roe <daniel@roe.dev>
2025-06-09 16:28:51 +0100

    feat: add `TypedURLSearchParams`

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export type { TypedHeaders, TypedRequest, TypedResponse } from './fetch'
 
 export type { HTTPMethod, MimeType, RequestHeaderMap, RequestHeaderName, RequestHeaders, ResponseHeaderMap, ResponseHeaderName, ResponseHeaders } from './http'
 
+export type { TypedURLSearchParams } from './http/url'
+
 export type { TypedFetchInput, TypedFetchPath, TypedFetchRequestInit, TypedFetchResponseBody as TypedFetchResponse, TypedFetchResponseBody } from './inference'
 
 export { serializeRoutes } from './serialize'
